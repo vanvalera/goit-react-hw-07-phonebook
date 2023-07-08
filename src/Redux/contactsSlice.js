ede11424a3cbc399ed766b152e4a898f3b0e086c
vanvalera <vmarton777@gmail.com>
2023-07-08 23:02:11 +0300

    fix_add_case

--- src/Redux/contactsSlice.js
+++ src/Redux/contactsSlice.js
@@ -16,7 +16,7 @@ const contactsSlice = createSlice({
         state.items = action.payload;
       })
       .addCase(addContacts.fulfilled, (state, action) => {
-        state.items.unshift(action.payload);
+        state.items.push(action.payload); // Изменено на push
       })
       .addCase(deleteContacts.fulfilled, (state, action) => {
         const index = state.items.findIndex(
